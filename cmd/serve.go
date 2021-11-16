abd99da70f1d1b9bd3012e6ffff6e7cb49f1fca3
Sam Lucidi <slucidi@redhat.com>
2021-11-15 22:58:34 -0500

    Set up handlers

--- cmd/serve.go
+++ cmd/serve.go
@@ -9,14 +9,30 @@ import (
 
 func main() {
 	db.Setup()
-	r := gin.Default()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
+	e := gin.Default()
+	e.Use(gin.Logger())
+	e.Use(gin.Recovery())
 
-	applicationHandler := handlers.ApplicationHandler{}
-	applicationHandler.AddRoutes(r)
+	handlerList := []handlers.Handler{
+		&handlers.ApplicationHandler{},
+		&handlers.BinaryRepoHandler{},
+		&handlers.BusinessServiceHandler{},
+		&handlers.GroupHandler{},
+		&handlers.JobFunctionHandler{},
+		&handlers.JobFunctionBindingHandler{},
+		&handlers.ReviewHandler{},
+		&handlers.RoleHandler{},
+		&handlers.RoleBindingHandler{},
+		&handlers.SourceRepoHandler{},
+		&handlers.TagHandler{},
+		&handlers.TagTypeHandler{},
+		&handlers.UserHandler{},
+	}
+	for _, h := range handlerList {
+		h.AddRoutes(e)
+	}
 
-	err := r.Run()
+	err := e.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
