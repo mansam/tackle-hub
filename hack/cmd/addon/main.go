40b1504e4d478801058e1ec3ceeeb17173388783
Jeff Ortel <jortel@redhat.com>
2021-12-07 10:57:44 -0800

    Added addon comments.

--- hack/cmd/addon/main.go
+++ hack/cmd/addon/main.go
@@ -1,10 +1,15 @@
+/*
+TEST addon adapter.
+This is an example of an addon adapter that lists files in /etc
+and creates an application artifact for each. Error handling is
+deliberately minimized to reduce code clutter.
+ */
 package main
 
 import (
 	"bytes"
 	"fmt"
 	hub "github.com/konveyor/tackle-hub/addon"
-	"os"
 	"os/exec"
 	"strings"
 	"time"
@@ -22,30 +27,52 @@ var (
 //
 // main
 func main() {
-	fmt.Printf("Started.")
+	//
+	// Task update: The addon has started.
+	// This MUST be called before reporting any
+	// other progress.
 	_ = addon.Started()
-	paths, err := list()
-	if err != nil {
-		_ = addon.Failed(err.Error())
-		os.Exit(-1)
-	}
+	//
+	// Find files.
+	paths, _ := find()
+	//
+	// Upload files and create artifacts.
 	upload(paths)
-	_ = addon.Activity("done")
+	//
+	// Task update: The addon has succeeded.
 	_ = addon.Succeeded()
 }
 
 //
 // upload artifacts.
 func upload(paths []string) {
+	//
+	// Get the addon data associated with the task.
 	d := &Data{}
 	_ = addon.DataWith(d)
+	//
+	// Task update: Update the task with total number of
+	// items to be processed by the addon.
 	_ = addon.Total(len(paths))
+	//
+	// Upload artifacts.
 	for _, p := range paths {
+		//
+		// Task update: The current addon activity.
 		_ = addon.Activity("uploading: ", p)
+		//
+		// Upload the file and create an artifact to be
+		// associated with the application.
 		_ = addon.Upload(d.Application, Kind, p)
 		pause()
+		//
+		// Task update: Increment the number of completed
+		// items processed by the addon.
 		_ = addon.Completed(1)
 	}
+	//
+	// Task update: update the current addon activity.
+	_ = addon.Activity("done")
 }
 
 //
@@ -55,8 +82,8 @@ func pause() {
 }
 
 //
-// list directory.
-func list() (paths []string, err error) {
+// find files.
+func find() (paths []string, err error) {
 	cmd := exec.Command(
 		"find",
 		"/etc",
