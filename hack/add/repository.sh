023c740d96872eddf18d0caba5f6ffccfd75de3b
Jeff Ortel <jortel@redhat.com>
2022-01-10 15:06:22 -0800

    Fixed repository hack script.

--- hack/add/repository.sh
+++ hack/add/repository.sh
@@ -8,7 +8,6 @@ curl -X POST ${host}/repositories -d \
     "application": 1,
     "name": "created-directly",
     "kind": "git",
-    "category": "source",
     "url": "git://github.com/testing"
 }' | jq -M .
 
@@ -17,6 +16,5 @@ curl -X POST ${host}/application-inventory/application/1/repositories -d \
     "createUser": "tackle",
     "name": "created-for-application",
     "kind": "git",
-    "category": "source",
     "url": "git://github.com/testing"
 }' | jq -M .
