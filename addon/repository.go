0380820d26497c1e8528a2e18cb043d7479e7585
Jeff Ortel <jortel@redhat.com>
2022-01-20 12:10:16 -0800

    Add addon.Repository.ByApplication().

--- addon/repository.go
+++ addon/repository.go
@@ -1,7 +1,9 @@
 package addon
 
 import (
+	"errors"
 	"github.com/konveyor/tackle-hub/api"
+	"net/http"
 	pathlib "path"
 	"strconv"
 )
@@ -22,6 +24,26 @@ func (h *Repository) Get(id uint) (m *api.Repository, err error) {
 			api.RepositoriesRoot,
 			strconv.Itoa(int(id))),
 		m)
+	return
+}
+
+//
+// ByApplication a repository by application ID.
+// id - The application ID.
+func (h *Repository) ByApplication(id uint) (m *api.Repository, err error) {
+	list := []api.Repository{}
+	err = h.client.Get(
+		pathlib.Join(
+			api.AppRepositoriesRoot,
+			strconv.Itoa(int(id))),
+		&list)
+	if err != nil {
+		return
+	}
+	if len(list) < 1 {
+		err = errors.New(http.StatusText(http.StatusNotFound))
+	}
+	m = &list[0]
 	return
 }
 
